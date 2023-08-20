077c48d63f1de577463cde208192ef1dd5a3393b
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-08-20 15:04:42 -0400

    gnostr-cli: initial commit

---
+++ src/pull_request.rs
@@ -0,0 +1,39 @@
+use nostr::{Event, EventBuilder, Keys };
+
+
+use crate::{ngit_tag::{tag_repo, tag_branch, tag_branch_merge_from, tag_hashtag, tag_into_event}, kind::Kind};
+
+pub fn initialize_pull_request(
+    keys: &Keys,
+    repository:&String,
+    to_branch_id: &String,
+    from_branch_id: &String,
+    title: &String,
+    description: &String,
+    hashtags: Vec<String>,
+
+) -> Event {
+    let mut tags = vec![
+        tag_repo(repository),
+        tag_into_event(tag_repo(repository)),
+        tag_branch(to_branch_id),
+        tag_into_event(tag_branch(to_branch_id)),
+        tag_branch_merge_from(from_branch_id),
+        tag_into_event(tag_branch_merge_from(from_branch_id)),
+        tag_hashtag("gnostr-event"),
+        tag_hashtag("gnostr-format-0.0.0"),
+];
+    for t in hashtags.iter() {
+        tags.push(
+            nostr::Tag::Hashtag(t.to_string())
+        )
+    }
+    EventBuilder::new(
+        Kind::PullRequest.into_sdk_custom_kind(),
+        format!("PullRequest\n{title}\n\n{description}"),
+        &tags,
+    )
+    .to_unsigned_event(keys.public_key())
+    .sign(&keys)
+    .unwrap()
+}
