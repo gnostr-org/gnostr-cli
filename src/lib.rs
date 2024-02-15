60e244cffdcd781f1a22c2c67f31bb3d539624f2
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-15 14:12:35 -0500

    1708024253/95b3c43/64b14a5-cli-experiment

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+#![cfg_attr(not(test), warn(clippy::pedantic))]
+#![cfg_attr(not(test), warn(clippy::expect_used))]
+
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+mod cli_interactor;
+mod client;
+mod config;
+mod git;
+mod key_handling;
+mod login;
+mod repo_ref;
+mod sub_commands;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+    /// nsec or hex private key
+    #[arg(short, long, global = true)]
+    nsec: Option<String>,
+    /// password to decrypt nsec
+    #[arg(short, long, global = true)]
+    password: Option<String>,
+    /// disable spinner animations
+    #[arg(long, action)]
+    disable_cli_spinners: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// save encrypted nsec for future use
+    Login(sub_commands::login::SubCommandArgs),
+    /// issue repository reference event as a maintainers
+    Init(sub_commands::init::SubCommandArgs),
+    /// send a PR / patch / patch set via nostr events
+    Send(sub_commands::send::SubCommandArgs),
+    /// list open PRs / patches / patch sets and pull / apply them a branch
+    List(sub_commands::list::SubCommandArgs),
+    /// pull latest commits in pr linked to checked out branch
+    Pull,
+    /// push commits to current checked out pr branch
+    Push,
+}
+
+#[tokio::main]
+async fn cli() -> Result<()> {
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Login(args) => sub_commands::login::launch(&cli, args).await,
+        Commands::Init(args) => sub_commands::init::launch(&cli, args).await,
+        Commands::Send(args) => sub_commands::send::launch(&cli, args).await,
+        Commands::List(args) => sub_commands::list::launch(&cli, args).await,
+        Commands::Pull => sub_commands::pull::launch().await,
+        Commands::Push => sub_commands::push::launch(&cli).await,
+    }
+}
